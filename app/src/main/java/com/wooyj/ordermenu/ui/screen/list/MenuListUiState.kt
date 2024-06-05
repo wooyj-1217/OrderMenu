1e007a0ee0c059cc1e37389b19d000aaa5bb97e4
우유진 <glem1004@gmail.com>
2024-06-05 18:23:05 +0900

    MenuListUiState.kt 추가

--- app/src/main/java/com/wooyj/ordermenu/ui/screen/list/MenuListUiState.kt
+++ app/src/main/java/com/wooyj/ordermenu/ui/screen/list/MenuListUiState.kt
@@ -2,9 +2,15 @@ package com.wooyj.ordermenu.ui.screen.list
 
 import com.wooyj.ordermenu.ui.screen.list.model.MenuTypeUI
 
-data class MenuListUiState(
-    val menuList: List<Menu>,
-)
+sealed class MenuListUiState {
+    data object None : MenuListUiState()
+
+    data object Loading : MenuListUiState()
+
+    data class Success(val menuList: List<Menu>) : MenuListUiState()
+
+    data class Error(val exception: Throwable) : MenuListUiState()
+}
 
 data class Menu(
     val header: Int,
