8c9511a4361b46d8786a0bd680703a24553a1988
우유진 <glem1004@gmail.com>
2024-06-19 20:35:52 +0900

    ApiUrl object keyword로 변경

--- app/src/main/java/com/wooyj/ordermenu/data/remote/api/ApiUrl.kt
+++ app/src/main/java/com/wooyj/ordermenu/data/remote/api/ApiUrl.kt
@@ -1,9 +1,7 @@
 package com.wooyj.ordermenu.data.remote.api
 
-class ApiUrl {
-    companion object {
-        const val API_BREED_LIST = "breeds/list/all"
-        const val API_BREED_IMAGE_LIST = "breed/{breedName}/images"
-        const val API_SUBBREED_IMAGE_LIST = "breed/{breedName}/{subBreedName}/images"
-    }
+object ApiUrl {
+    const val API_BREED_LIST = "breeds/list/all"
+    const val API_BREED_IMAGE_LIST = "breed/{breedName}/images"
+    const val API_SUBBREED_IMAGE_LIST = "breed/{breedName}/{subBreedName}/images"
 }
