daa6f62b05a730022ce97f1fd89d708728fdd157
우유진 <glem1004@gmail.com>
2024-06-26 20:44:25 +0900

    질문 빠뜨린거 추가

--- app/src/main/java/com/wooyj/ordermenu/data/local/room/AppDatabase.kt
+++ app/src/main/java/com/wooyj/ordermenu/data/local/room/AppDatabase.kt
@@ -11,6 +11,7 @@ import com.wooyj.ordermenu.data.local.room.entity.FavoriteDogEntity
 
 private const val DATABASE_VERSION = 1
 
+// TODO("실무에서 room으로 캐시데이터처럼 사용하는 경우가 있나요?")
 @Database(version = DATABASE_VERSION, entities = [BreedEntity::class, FavoriteDogEntity::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun breedDao(): BreedDAO
